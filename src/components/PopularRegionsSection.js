11b6f318038d23d55798f935d1b02ea50beabe9a
Jessica Aprilia Putri <jessicaputri2005@icloud.com>
2025-10-21 23:19:21 +0800

    refactor(data): move regions dataset to separate file for PopularRegionsSection

--- src/components/PopularRegionsSection.js
+++ src/components/PopularRegionsSection.js
@@ -1,15 +1,6 @@
 import React from "react";
 import RegionItem from "./RegionItem"; // Import the reusable region card
-
-// static data for the six popular regions (from your original HTML)
-const regions = [
-  { name: "Rhovanion", imageSrc: "http://placehold.it/194x194" },
-  { name: "Eriador", imageSrc: "http://placehold.it/194x194" },
-  { name: "Bay of Belfalas", imageSrc: "http://placehold.it/194x194" },
-  { name: "Mordor", imageSrc: "http://placehold.it/194x194" },
-  { name: "The Southwest", imageSrc: "http://placehold.it/194x194" },
-  { name: "Arnor", imageSrc: "http://placehold.it/194x194" },
-];
+import regions from "../data/regions"; // Import dataset
 
 // Section component to render all regions on the Home page
 function PopularRegionsSection() {
---
+++ src/data/regions.js
@@ -0,0 +1,13 @@
+// Dataset for the "Popular Regions" section
+
+const regions = [
+  { name: "Rhovanion", imageSrc: "http://placehold.it/194x194" },
+  { name: "Eriador", imageSrc: "http://placehold.it/194x194" },
+  { name: "Bay of Belfalas", imageSrc: "http://placehold.it/194x194" },
+  { name: "Mordor", imageSrc: "http://placehold.it/194x194" },
+  { name: "The Southwest", imageSrc: "http://placehold.it/194x194" },
+  { name: "Arnor", imageSrc: "http://placehold.it/194x194" },
+];
+
+// Export regions
+export default regions;
