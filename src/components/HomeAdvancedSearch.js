0507027fd2df30b910149b5f767602c8bdb5d858
Jessica Aprilia Putri <jessicaputri2005@icloud.com>
2025-10-24 18:57:40 +0800

    refactor(HomeAdvancedSearch.js): move night and bedroom sections outside return and add style

--- src/components/HomeAdvancedSearch.js
+++ src/components/HomeAdvancedSearch.js
@@ -1,9 +1,52 @@
 import React from "react";
 import DateInput from "./DateInput"; // adjust the import path
 
-// create a HomeAdvancedSearch Function Component
 function HomeAdvancedSearch() {
-  // return the HomeAdvancedSearch (from .html code)
+  // style for stay night and bedroom
+  const paddingStyle = {
+    padding: "20px",
+  };
+
+  // map stay night options
+  const nightOptions = (
+    <div className="form-control-small" style={paddingStyle}>
+      <select
+        id="search_status"
+        name="search_status"
+        data-placeholder="Stay..."
+        className="form-control"
+      >
+        <option value="">Stay...</option>
+        {/* array map the options */}
+        {[...Array(14)].map((_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1} Night{i > 0 ? "s" : ""}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
+  // map bedroom options
+  const bedroomOptions = (
+    <div className="form-control-small" style={paddingStyle}>
+      <select
+        id="search_bedrooms"
+        name="search_bedrooms"
+        data-placeholder="Bedrooms"
+        className="form-control"
+      >
+        <option value="">Bedrooms</option>
+        {/* array map the options */}
+        {[0, 1, 2, 3, 4, 5, "5+"].map((val, i) => (
+          <option key={i} value={val}>
+            {val}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
   return (
     <div id="home-advanced-search" className="open">
       <div id="opensearch"></div>
@@ -14,50 +57,27 @@ function HomeAdvancedSearch() {
               <div className="form-group">
                 {/* Arrival Date */}
                 <div className="form-control-small">
-                   <div className="input-group date chzn-container" data-datepicker>
-                      <DateInput />
-                      <span
-                        className="input-group-addon"
-                        onClick={() =>
-                          document.querySelector('input[type="date"]')?.showPicker?.()
-                        }
-                      >
-                        <span className="glyphicon glyphicon-calendar"></span>
-                      </span>
-                    </div>
+                  <div
+                    className="input-group date chzn-container"
+                    data-datepicker
+                  >
+                    <DateInput />
+                    <span
+                      className="input-group-addon"
+                      onClick={() =>
+                        document
+                          .querySelector('input[type="date"]')
+                          ?.showPicker?.()
+                      }
+                    >
+                      <span className="glyphicon glyphicon-calendar"></span>
+                    </span>
+                  </div>
                 </div>
 
-                {/* Nights */}
-                <div className="form-control-small">
-                  <select
-                    id="search_status"
-                    name="search_status"
-                    data-placeholder="Stay..."
-                  >
-                    <option value="">Stay...</option>
-                    {[...Array(14)].map((_, i) => (
-                      <option key={i + 1} value={i + 1}>
-                        {i + 1} Night{i > 0 ? "s" : ""}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-
-                {/* Bedrooms */}
-                <div className="form-control-small">
-                  <select
-                    id="search_bedrooms"
-                    name="search_bedrooms"
-                    data-placeholder="Bedrooms"
-                  >
-                    <option value="">Bedrooms</option>
-                    {[0, 1, 2, 3, 4, 5, "5+"].map((val, i) => (
-                      <option key={i} value={val}>
-                        {val}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                {/* Nights + Bedrooms */}
+                {nightOptions}
+                {bedroomOptions}
 
                 {/* Location */}
                 <div className="form-control-large">
@@ -69,6 +89,7 @@ function HomeAdvancedSearch() {
                   />
                 </div>
 
+                {/* Submit */}
                 <button type="submit" className="btn btn-fullcolor">
                   Search
                 </button>
@@ -81,5 +102,4 @@ function HomeAdvancedSearch() {
   );
 }
 
-// Export HomeAdvancedSearch so can be Import in App.js
 export default HomeAdvancedSearch;
