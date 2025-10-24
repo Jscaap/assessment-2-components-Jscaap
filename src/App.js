2ea705583d53361981efe3c151b26784368e2d0f
Jessica Aprilia Putri <jessicaputri2005@icloud.com>
2025-10-24 18:21:30 +0800

    feat(router): set up React Router and update NavBar links for SPA navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-import HomePage from "./pages/HomePage"; // import home page
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // import router
+// Import pages
+import HomePage from "./pages/HomePage";
+import NewsPage from "./pages/NewsPage";
+import RegionsPage from "./pages/RegionsPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      {/* define routes */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/regions" element={<RegionsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom"; // import Link for SPA navigation
 
-// create a NavBar Function Component
+// NavBar Functional Component
 function NavBar() {
-    // return the NavBar (from .html code)
-    return (
-        <div id="nav-section">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <a href="index.html" className="nav-logo">
-                            <img src="images/logo.png" alt="One Ring Rentals" />
-                        </a>
+  return (
+    <div id="nav-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            {/* Logo */}
+            <Link to="/" className="nav-logo">
+              <img src="/images/logo.png" alt="One Ring Rentals" />
+            </Link>
 
-                        <div id="sb-search" className="sb-search">
-                            <form>
-                            <input
-                                className="sb-search-input"
-                                placeholder="Search..."
-                                type="text"
-                                name="search"
-                                id="search"
-                            />
-                            <input className="sb-search-submit" type="submit" value="" />
-                            <i className="fa fa-search sb-icon-search"></i>
-                            </form>
-                        </div>
-
-                        <nav className="navbar">
-                            <button id="nav-mobile-btn">
-                                <i className="fa fa-bars"></i>
-                            </button>
-                            <ul className="nav navbar-nav">
-                                <li><a href="index.html">Find a Rental</a></li>
-                                <li><a href="#">List your rental</a></li>
-                                <li><a href="#">Travel Guides</a></li>
-                                <li><a href="regions.html">Regions</a></li>
-                                <li><a href="news.html">News</a></li>
-                                <li><a href="contact.html">Contact</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
+            {/* Search box */}
+            <div id="sb-search" className="sb-search">
+              <form>
+                <input
+                  className="sb-search-input"
+                  placeholder="Search..."
+                  type="text"
+                  name="search"
+                  id="search"
+                />
+                <input className="sb-search-submit" type="submit" value="" />
+                <i className="fa fa-search sb-icon-search"></i>
+              </form>
             </div>
+
+            {/* Navigation menu */}
+            <nav className="navbar">
+              <button id="nav-mobile-btn">
+                <i className="fa fa-bars"></i>
+              </button>
+              <ul className="nav navbar-nav">
+                <li><Link to="/regions">Regions</Link></li>
+                <li><Link to="/news">News</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+              </ul>
+            </nav>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-// Export NavBar so can be Import in App.js
+// export NavBar
 export default NavBar;
